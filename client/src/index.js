85b0b44e110452e4ef8bdda0975c635e21f7d866
gironalex <giron.alex@outlook.com>
2021-09-29 12:33:56 -0400

    Deleted files + Added CSS Styles sheets

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,20 @@
+/*************************************************
+ Treehouse FSJS TechDegree:
+ Project 10 - Full Stack React App with a REST API
+**************************************************/
+
+// Import Modules
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
+// Import Styles
+import '../src/styles/reset.css';
+import '../src/styles/global.css';
+
+// Import Main Component
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,7 +23,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
